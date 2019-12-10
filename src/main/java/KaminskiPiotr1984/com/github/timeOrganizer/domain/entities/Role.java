c36887dcd9d6aa7b9be39cb76bd2adfd35b89656
KaminskiPiotr1984 <kaminskipiotr1984@gmail.com>
2019-12-10 16:35:36 +0100

    Class: User, Role

---
+++ src/main/java/KaminskiPiotr1984/com/github/timeOrganizer/domain/entities/Role.java
@@ -0,0 +1,25 @@
+package KaminskiPiotr1984.com.github.timeOrganizer.domain.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = "id")
+public class Role {
+
+    @Id
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+}
