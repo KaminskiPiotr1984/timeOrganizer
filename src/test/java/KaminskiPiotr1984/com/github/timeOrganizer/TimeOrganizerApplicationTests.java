603d6dab270e3555f9fe402b0be32ec0830375aa
KaminskiPiotr1984 <kaminskipiotr1984@gmail.com>
2019-12-12 16:05:45 +0100

    tests

---
+++ src/test/java/KaminskiPiotr1984/com/github/timeOrganizer/TimeOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package KaminskiPiotr1984.com.github.timeOrganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
